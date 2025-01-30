5f9cbb05068a8ec480a8d27d772f9a5023179cdd
shlomo <shlomisal99@gmail.com>
2025-01-31 01:22:47 +0200

    deployment settings fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/shlomoHi99/digital_closet",
+  base: "/digital_closet/",
 });
